c0d60fc63350c8df7ad08641d70f80e79cf8265d
DharaniTharan06 <duraisamydharanitharan@gmail.com>
2026-02-16 21:51:59 +0530

    file_upload

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,24 @@
+import {v2} from "cloudinary"
+import fs from "fs"
+
+v2.config({
+    cloud_name: process.env.CLOUD_NAME,
+    api_key: process.env.CLOUD_API,
+    api_secret: process.env.CLOUD_SECRET
+});
+
+const uploadtocloud = async (filepath) => {
+    try {
+        if(!filepath) return null
+        const response = await v2.uploader.upload(filepath ,{
+            resource_type: "auto"
+        })
+        console.log("File is uploaded ",response)
+        return response
+    } catch (err) {
+        fs.unlinkSync(filepath)
+        return null
+    }
+} 
+
+export {uploadtocloud}
