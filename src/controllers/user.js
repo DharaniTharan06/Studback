d065e2cf78d00bec1249760195e7b539516cd8e5
DharaniTharan06 <duraisamydharanitharan@gmail.com>
2026-03-06 23:22:07 +0530

    Route and controller logic

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -1,6 +1,30 @@
+import { apiError } from "../utils/apiError.js";
 import asyncHandler from "../utils/asyncHandler.js";
+import {User} from "../models/user.js"
 
 const registeruser = asyncHandler(async(req,res)=>{
+
+    const {fullName , email , username , password} = req.body
+    console.log(`email : ${email}`);
+
+    if(
+        [fullName , email , username , password].some((field)=>
+        field?.trim === "")
+    ){
+        throw new apiError(400,"Field Empty")
+    } 
+
+    const existedUser = User.findOne({
+        $or: [{ username } ,{ email } ]
+    })
+
+    if(existedUser){
+        throw new apiError(409, "User already Exists")
+    }
+        
+    const avatarLocalPath = req.files?.avatar[0]?.path;
+    const coverImageLocalPath = req.files?.coverimage[0]?.path;
+
     res.status(200).json({
         message: "First testing of postman"
     })
--- src/routes/user.js
+++ src/routes/user.js
@@ -1,9 +1,22 @@
 import {Router} from "express"
 import registeruser from "../controllers/user.js"
+import {upload} from "../middlewares/multer.js"
 
 const router = Router()
 
 //here the route have the prefix /api/v1/users and we add route to this url
-router.route("/register").post(registeruser) // this give /api/v1/users/register and in this url we have register logic
+router.route("/register").post(
+    upload.fields([ //This is the middle ware which is added before the controller
+        {
+            name: "avatar",
+            maxCount: 1
+        },
+        {
+            name:"coverImage",
+            maxCount: 3
+        }
+    ]),
+    registeruser
+) // this give /api/v1/users/register and in this url we have register logic
 
 export default router
