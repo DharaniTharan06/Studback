9c31a35ca877630f93626a2fced353de01b5a9b7
DharaniTharan06 <duraisamydharanitharan@gmail.com>
2026-06-15 10:22:13 +0530

    Healthcare todo complete

--- src/controllers/healthcheck.js
+++ src/controllers/healthcheck.js
@@ -6,7 +6,13 @@ import asyncHandler from "../utils/asyncHandler.js"
 const healthcheck = asyncHandler(async (req, res) => {
     return res
     .status(200)
-    .json(new apiResponse(200,"Ok","Healthcheck is complete"))
+    .json(new apiResponse(
+        200,
+        {
+            status: "Ok"
+        },
+        "Healthcheck is complete"
+    ))
 })
 
 export {healthcheck}
